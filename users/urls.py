a1b271257721cf7fac6fa326cfb315078d65d652
joxacode <shohjahonhamidov192@gmail.com>
2023-08-31 12:20:23 +0500

    swagger

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.SignInAPIView.as_view()),
-    path('branch-user/', views.BranchListAPIView.as_view()),
-    path('branch-list/', views.UserBranchSerializer.as_view()),
+    path('branch-user/', views.UserBranchSerializer.as_view()),
+    path('branch-list/', views.BranchListAPIView.as_view()),
     path('reset-password/', views.ResetPasswordAPIView.as_view()),
     path('forgot-password/', views.ForgotPasswordCreateAPIView.as_view()),
     path('get-username/', views.GetUserNameCreateAPIView.as_view()),
